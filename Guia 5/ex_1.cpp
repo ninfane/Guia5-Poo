a22a915c727339b0261158de95d6be338a71697c
ninfane <marinacrespog@gmail.com>
2021-10-22 13:11:20 -0300

    Correccion ex. reemplazar por promedio

--- Guia 5/ex_1.cpp
+++ Guia 5/ex_1.cpp
@@ -12,6 +12,8 @@ int main() {
 		return 1; 
 	}
 	
+	//leo cada palabra del archivo de texto y lo llevo
+	//a mi vector
 	vector<string> v;
 	string x;
 	int contador = 0;
@@ -25,12 +27,16 @@ int main() {
 	//ordenar alfabeticamente el vector
 	sort(v.begin(),v.end());
 	
+	//cierro el archivo
 	ain.close();
 	
+	//escribo mi vector ordenado en el archivo 
 	ofstream aout("palabras.txt", ios::trunc);
 	for(size_t i=0;i<v.size();i++) { 
 		aout << v[i] << endl;
 	}
+	//no es necesario aclarar el close en este caso
+	aout.close();
 	
 	return 0;
 }
